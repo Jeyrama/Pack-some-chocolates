95d5a52f889ef64d0149762e3f64abc1b3aecc4b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-08 21:56:06 -0700

    Update Pack-some-chocolates.js

--- Pack-some-chocolates.js
+++ Pack-some-chocolates.js
@@ -34,3 +34,12 @@ Example:
 
 
 // Solution
+
+function makeChocolates(small, big, goal) {
+  for (let b = big; b >= 0; b--){
+    for (let s = small; s >= 0; s--){
+      if ((b*5 + s*2) === goal) return s;
+    }
+  }
+  return -1;
+}
